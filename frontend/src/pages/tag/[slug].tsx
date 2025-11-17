fe0be32191be5eab4128d1d26559029629ce6dc2
Humanshu Jaglan <humanshujaglan@gmail.com>
2025-11-17 17:29:58 +0530

    added district

--- frontend/src/pages/tag/[slug].tsx
+++ frontend/src/pages/tag/[slug].tsx
@@ -15,7 +15,7 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { ArrowDownUp, ArrowRight, CalendarDays, Eye, ListFilter, Search } from "lucide-react";
+import { ArrowDownUp, ArrowRight, CalendarDays, ChevronDown, ChevronUp, Eye, ListFilter, Search } from "lucide-react";
 import AdBanner from "@/components/shared/AdBanner";
 import Image from "next/image";
 import { useEffect, useMemo, useState } from "react";
@@ -268,6 +268,89 @@ const CategoryPage: NextPage<CategoryPageProps> = ({ category, posts, categories
           </div>
 
           <div>
+            {
+              category.name === 'haryana' &&
+              <div className="bg-white ">
+          {(() => {
+            const states = [
+              "Ambala",
+              "Kurukshetra",
+              "Panchkula",
+              "Yamunanagar",
+              "Faridabad",
+              "Nuh",
+              "Palwal",
+              "Gurugram",
+              "Mahendragarh",
+              "Rewari",
+              "Hisar",
+              "Fatehabad",
+              "Jind",
+              "Sirsa",
+              "Karnal",
+              "Kaithal",
+              "Panipat",
+              "Rohtak",
+              "Bhiwani",
+              "Charkhi Dadri",
+              "Jhajjar",
+              "Sonipat"
+            ];
+
+            const colors = [
+              "from-[#9B2821] to-[#EB2139]",
+              "from-[#0C342B] to-[#1D6F50]",
+              "from-[#1c1e47] via-[#2b2d6c] to-[#34387e]",
+            ];
+
+            return (
+              <>
+                <div className="grid grid-cols-3 sm:grid-cols-6 gap-2">
+                  <input
+                    id="toggle-states"
+                    type="checkbox"
+                    title="Show More"
+                    className="hidden peer"
+                  />
+
+                  {states.map((state, idx) => (
+                    <Link
+                      key={state}
+                      href={`/tag/${encodeURIComponent(state.toLowerCase().replace(/\s+/g, '-'))}`}
+                      className={`px-3 mb-0 py-2 rounded-lg flex-1 text-center text-sm font-medium transition-colors bg-gradient-to-b text-white truncate
+                        ${colors[idx % colors.length]}
+                        ${idx >= 6 && idx < 12 ? "hidden lg:block peer-checked:block" : ""}
+                        ${idx >= 12 ? "hidden peer-checked:block" : ""}
+                      `}
+                    >
+                      {state}
+                    </Link>
+                  ))}
+                  {states.length > 6 && (
+                    <>
+                      <label
+                        htmlFor="toggle-states"
+                        className="mt-4 col-span-full justify-center hidden items-center gap-2 px-4 py-2 rounded-full text-sm text-black cursor-pointer select-none peer-checked:inline-flex transition-transform duration-300"
+                      >
+                          View Less
+                          <ChevronUp className="w-4 h-4" />
+                      </label>
+                      <label
+                        htmlFor="toggle-states"
+                        className="mt-4 col-span-full justify-center inline-flex items-center gap-2 px-4 py-2 rounded-full text-sm text-black cursor-pointer select-none peer-checked:hidden transition-transform duration-300"
+                      >
+                          View More
+                          <ChevronDown className="w-4 h-4" />
+                      </label>
+                    </>
+                  )}
+                </div>
+              </>
+            );
+          })()}
+        </div>
+
+            }
             {posts.length === 0 ? (
               <Card>
                 <CardContent className="py-12 text-center">
